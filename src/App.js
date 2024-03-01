d4c70ad3776cd73c3d14bdd59b27a3988c3d3a7b
Andreas Witten Honoré <honore.andreas@gmail.com>
2024-03-01 10:21:40 +0100

    Setup layout of index page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = 'https://api.openweathermap.org/data/2.5/weather?lat=55.39594&lon=10.38831&appid=2438ec868e96bc0d041dc6fde565f0b6';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Odense</p>
+          </div>
+          <div className="temp">
+            <h1>15°C</h1>
+          </div>
+          <div className="description">
+            <p>Overskyet</p>
+          </div>
+        </div>
+        <div className="bottom">
+            <div className="feels">
+              <p className='bold'>12°C</p>
+              <p>Føles som</p>
+            </div>
+            <div className="humidity">
+              <p className='bold'>80%</p>
+              <p>Luftfugtighed</p>
+            </div>
+            <div className="wind">
+              <p className='bold'>10 km/t</p>
+              <p>Vindhastighed</p>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
